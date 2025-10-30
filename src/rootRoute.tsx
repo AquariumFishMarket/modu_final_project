cbe584a3a65ac794c91a5f833d5589356cdb1909
Yeseul Kim <dptmfl3018@naver.com>
2025-10-30 14:08:06 +0900

    [#18] 메인 로그인 및 레이아웃 수정

--- src/rootRoute.tsx
+++ src/rootRoute.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import GlobalLayout from "./components/layout/globalLayout";
+import Login from "./components/pages/Login";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
         <Route path="/" />
-        <Route path="/login" />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/email" />
         <Route path="/signup" />
         <Route path="/profile-setting" />
 
