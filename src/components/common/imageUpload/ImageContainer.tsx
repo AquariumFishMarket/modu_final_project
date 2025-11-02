1f73398179412b49be5ba6eef72c0d0874b3a260
Yeseul Kim <dptmfl3018@naver.com>
2025-11-02 20:43:23 +0900

    [#54] 프로필 이미지 UI 수정, 업로드 버튼 타입 추가

--- src/components/common/imageUpload/ImageContainer.tsx
+++ src/components/common/imageUpload/ImageContainer.tsx
@@ -1,81 +1,80 @@
 import { Dispatch, SetStateAction, useState } from "react";
 import DeleteButton from "./DeleteButton";
-import styled from "styled-components"
+import styled from "styled-components";
 
 interface imgcontainer {
-    type: 'post' | 'profile';
-    setDeleteIdx: Dispatch<SetStateAction<number | undefined>>;
-    imgArr: File[]
+  type: "post" | "profile";
+  setDeleteIdx: Dispatch<SetStateAction<number | undefined>>;
+  imgArr: File[];
 }
 
 //이미지 여러개일 때
 const ContainerArr = styled.ul`
-    display: flex;
-    width: calc(100% - 90px);
-    overflow-x: auto;
-`
+  display: flex;
+  width: calc(100% - 90px);
+  overflow-x: auto;
+`;
 const ImageListArr = styled.li`
-    position: relative;
-    width: 80px;
-    border-radius: 10px;
-    overflow: hidden;
-    flex: 0 0 auto;
-    aspect-ratio: 1;
-    margin-right: 8px;
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`
-const ContainerProfile = styled.div`
+  position: relative;
+  width: 80px;
+  border-radius: 10px;
+  overflow: hidden;
+  flex: 0 0 auto;
+  aspect-ratio: 1;
+  margin-right: 8px;
+  img {
     width: 100%;
     height: 100%;
-`
+    object-fit: cover;
+  }
+`;
+const ContainerProfile = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 const ImageSolo = styled.div`
-    position: relative;
+  position: relative;
+  height: 100%;
+  img {
+    width: 100%;
     height: 100%;
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`
+    object-fit: cover;
+    border-radius: 100%; // 이미지 적용 후 동글동글
+  }
+`;
 
-export default function ImageContainer({type,imgArr,setDeleteIdx}:imgcontainer) {
-    if(type === 'post') {
-        return (
-            <ContainerArr>
-                {
-                    imgArr.map((imgele,i)=>(
-                        <ImageListArr key={i}>
-                            <img
-                            src={URL.createObjectURL(imgele)}
-                            alt={`preview-${i}`}
-                            ></img>
-                            <DeleteButton data-index={i} setDeleteIdx={setDeleteIdx}/>
-                        </ImageListArr>
-                    ))
-                }
-            </ContainerArr>
-        )
-    }
-    if(type === 'profile') {
-        const LastImageIdx = imgArr[imgArr.length - 1];
-        return (
-            <>
-            {LastImageIdx && (
-                <ContainerProfile>
-                    <ImageSolo>
-                        <img
-                        src={URL.createObjectURL(LastImageIdx)}
-                        alt={`나의 프로필`}
-                        ></img>
-                    </ImageSolo>
-                </ContainerProfile>
-            )}
-            </>
-        )
-    }
-
+export default function ImageContainer({
+  type,
+  imgArr,
+  setDeleteIdx,
+}: imgcontainer) {
+  if (type === "post") {
+    return (
+      <ContainerArr>
+        {imgArr.map((imgele, i) => (
+          <ImageListArr key={i}>
+            <img src={URL.createObjectURL(imgele)} alt={`preview-${i}`}></img>
+            <DeleteButton data-index={i} setDeleteIdx={setDeleteIdx} />
+          </ImageListArr>
+        ))}
+      </ContainerArr>
+    );
+  }
+  if (type === "profile") {
+    const LastImageIdx = imgArr[imgArr.length - 1];
+    return (
+      <>
+        {LastImageIdx && (
+          <ContainerProfile>
+            <ImageSolo>
+              <img
+                src={URL.createObjectURL(LastImageIdx)}
+                alt={`나의 프로필`}
+              ></img>
+            </ImageSolo>
+          </ContainerProfile>
+        )}
+      </>
+    );
+  }
 }
