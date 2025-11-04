3cfc6399f1b9cc305a945a2ff9188a1d3284fbf0
yeaseula <kminea@naver.com>
2025-11-04 16:34:10 +0900

    [#77] footer lottie 애니메이션 변경

--- src/components/common/FooterNav.tsx
+++ src/components/common/FooterNav.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import { defineElement } from "@lordicon/element";
-import { NavLinkRenderProps } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { useRef, useEffect } from "react";
+
+import { NavLinkRenderProps, useLocation } from "react-router-dom";
+import HOME from '../../json/home.json'
+import HOMEACTIVE from '../../json/home-active.json'
+import CHAT from '../../json/chat.json'
+import CHATACTIVE from '../../json/chat-active.json'
+import WRITE from '../../json/write.json'
+import WRITEACTIVE from '../../json/write-active.json'
+import PROFILE from '../../json/profile.json'
+import PROFILEACTIVE from '../../json/profile-active.json'
+
 import {
   NavContainer,
   NavList,
@@ -13,40 +24,43 @@ import {
 interface NavItemType {
   label: string;
   path: string;
-  icon: string;
-  activeIcon?: string;
+  icon: Record<string,any>;
+  activeIcon?: Record<string,any>;
 }
 
 const navItems: NavItemType[] = [
   {
     label: "홈",
     path: "/",
-    icon: "/src/assets/icons/icon-home.svg",
-    activeIcon: "/src/assets/icons/icon-home-fill.svg",
+    icon: HOME,
+    activeIcon: HOMEACTIVE,
   },
   {
     label: "채팅",
     path: "/chat" /* 수정 필요 */,
-    icon: "/src/assets/icons/icon-message-circle.svg",
-    activeIcon: "/src/assets/icons/icon-message-circle-fill.svg",
+    icon: CHAT,
+    activeIcon: CHATACTIVE,
   },
   {
     label: "게시물 작성",
     path: "/post" /* 수정 필요 */,
-    icon: "/src/assets/icons/icon-edit.svg",
+    icon: WRITE,
+    activeIcon: WRITEACTIVE
   },
   {
     label: "프로필",
     path: "/profile" /* 수정 필요 */,
-    icon: "/src/assets/icons/icon-user.svg",
-    activeIcon: "/src/assets/icons/icon-user-fill.svg",
+    icon: PROFILE,
+    activeIcon: PROFILEACTIVE,
   },
 ];
-defineElement();
+
 const FooterNav = () => {
+  const { pathname } = useLocation()
+  const HomeRef = useRef<Player>(null)
+
   return (
     <NavContainer>
-      <lord-icon trigger="hover" src="/my-icon.json"></lord-icon>
       <NavList>
         {navItems.map((item: NavItemType) => (
           <NavItem key={item.path}>
@@ -54,10 +68,12 @@ const FooterNav = () => {
               {({ isActive }: NavLinkRenderProps): React.ReactNode => (
                 <>
                   <IconWrapper>
-                    <img
-                      src={isActive ? item.activeIcon || item.icon : item.icon}
-                      alt={item.label}
-                    />
+                  <Player
+                  ref={HomeRef}
+                  src={isActive ? item.activeIcon || item.icon : item.icon}
+                  autoplay={isActive ? true || false : false}
+                  style={{ width: '30px', height: '30px' }}
+                  />
                   </IconWrapper>
                   <NavLabel>{item.label}</NavLabel>
                 </>
