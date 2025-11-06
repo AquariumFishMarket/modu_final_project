21618267151eec26344e6312dacd1462b17ed18a
qwezxc3810 <qwezxc3810@gmail.com>
2025-11-06 11:11:54 +0900

    [#89] feat: 스플래시 화면 및 페이지 전환 애니메이션 구현

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -87,7 +87,7 @@ function Login() {
     animate: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.5, ease: [0.22, 1, 0.36, 1] }
+      transition: { duration: 0.5 }
     },
     exit: {
       opacity: 0,
