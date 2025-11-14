63e0c7b0ebb65f51a9ba222861c09b92e08e5e9c
qwezxc3810 <qwezxc3810@gmail.com>
2025-11-14 12:43:49 +0900

    알수없는 오류 자꾸나네요..

--- src/pages/Home/FeedPage.tsx
+++ src/pages/Home/FeedPage.tsx
@@ -14,6 +14,7 @@ import { useFeedStore } from "../../contexts/useFeedStore";
 const FeedPage = () => {
   const navigate = useNavigate();
 
+
   const toggleLike = useFeedStore((state) => state.toggleLike);
 
   const observer = useRef<IntersectionObserver | null>(null);
