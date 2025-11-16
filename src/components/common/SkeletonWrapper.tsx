1b2de69a0898af261c772a59e8f12de907b364c8
yeaseula <kminea@naver.com>
2025-11-17 00:02:56 +0900

    [#219] 기본 스켈레톤 컴포넌트 생성

---
+++ src/components/common/SkeletonWrapper.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { Skeleton } from "./Skeleton";
+
+interface SkeletonProps{
+    width: number;
+    height: number;
+    marginTop?: number;
+    marginBottom?: number;
+    marginLeft?: number;
+    marginRight?: number;
+}
+
+interface SkeletonStyleProps {
+    $width: number;
+    $height: number;
+    $marginTop?: number;
+    $marginBottom?: number;
+    $marginLeft?: number;
+    $marginRight?: number;
+}
+
+const SkeletonContainer = styled.div<SkeletonStyleProps>`
+    width: ${({ $width }) => $width}px;
+    height: ${({ $height }) => $height}px;
+    margin-top: ${({ $marginTop }) => $marginTop ?? 0}px;
+    margin-bottom: ${({ $marginBottom }) => $marginBottom ?? 0}px;
+    margin-left: ${({ $marginLeft })=> $marginLeft ?? 0}px;
+    margin-right: ${({ $marginRight })=> $marginRight ?? 0}px;
+    overflow: hidden;
+`
+
+export default function SkeletonWrapper({...props}:SkeletonProps) {
+    return(
+        <SkeletonContainer
+            $width={props.width}
+            $height={props.height}
+            $marginTop={props.marginTop}
+            $marginBottom={props.marginBottom}
+            $marginLeft={props.marginLeft}
+            $marginRight={props.marginRight}
+        >
+            <Skeleton />
+        </SkeletonContainer>
+    )
+}
